c19df9cb6412cb310f0645009d6e2650d7039309
AmmarAhmed1448 <ammarahmed1448@gmail.com>
2024-01-15 23:55:20 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from config import app, api, mongo
+from flask_restful import Resource, reqparse
+from werkzeug.security import generate_password_hash, check_password_hash
+from adminRegister import registerAdmin 
+from adminDashboard import AdminDashboard
+from login import Login
+from policy import TimeInterval, Days, SalaryDeduction
+from attendance import MarkAttendance
+from employee import registerEmployee, ChangePassword
+
+from getDeductedSalary import getDeductedSalary_bp
+from markAttendance import markAttendance_bp
+from modelTester import modelTester_bp
+from markAbsent import markAbsent_bp
+
+# index page or default page        
+@app.route("/")
+def home_page():
+    return "Default page"
+
+# endpoints
+api.add_resource(registerAdmin, '/registerAdmin')
+api.add_resource(Login, '/login')
+api.add_resource(TimeInterval, '/policy/setTimeInterval')
+api.add_resource(MarkAttendance, '/attendance')
+api.add_resource(registerEmployee, '/registerEmployee')
+api.add_resource(ChangePassword, '/ChangePassword')
+
+# Ammar's code
+api.add_resource(AdminDashboard, '/admin/dashboard')
+api.add_resource(Days, '/policy/setDays')
+api.add_resource(SalaryDeduction, '/policy/setSalaryDeduction')
+
+app.register_blueprint(getDeductedSalary_bp)
+app.register_blueprint(markAttendance_bp)
+app.register_blueprint(modelTester_bp)
+app.register_blueprint(markAbsent_bp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+api = Api(app)
+
+# Configure Flask-PyMongo
+# app.config["MONGO_URI"] = "mongodb+srv://Ayesha:Ayesha123@facelog.ecludwz.mongodb.net/FacelogDB"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/FaceLog"
+mongo = PyMongo(app)
